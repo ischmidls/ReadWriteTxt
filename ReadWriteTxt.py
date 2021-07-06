f7c289bb56a62b637a0dde41776771745a97bdc1
ischmidls <izak.schmidlkofer@gmail.com>
2021-07-05 19:06:47 -0700

    commit

--- ReadWriteTxt.py
+++ ReadWriteTxt.py
@@ -1,40 +1,71 @@
 # Author: Izak Schmidlkofer
 # Date(s): 2021
-# Description: class File represents a file; 
+# Description: class TxtFile represents a text file;
 
 
-
-class File:
-  """
-  Represents a C
-  Attributes: file_name: string
-  Methods: f()
-  """
-  
-  def __init__(self, file_name: string):
-    """Returns an object"""
-
-    
-  
-  def f(self):
+class TxtFile:
     """
-    Takes 
-    Returns
+    Represents a text file
+    Attributes: file_name: string
+    Methods: f()
     """
 
-    return 
+    def __init__(self, file_name='test.txt'):
+        """Returns a text file object"""
+
+        self.file_name = file_name
+        self.file_list = []
+
+    def text_list(self):
+        """
+        Read file to list
+
+        Takes None
+        Returns None
+        """
+
+        with open(self.file_name, 'r') as file:
+            for line in file:
+                self.file_list.append(line.strip())
+
+    def f(self):
+        """
+        Takes
+        Returns
+        """
+
+        return
+
+    def write_text(self):
+        """
+        Write list to file
+
+        Takes None
+        Returns None
+        """
+
+        with open("new_text.txt", 'w') as file:
+            for line in self.file_list:
+                file.write(f"{line}\n")
 
 
 def main():
-  """
-  The program calls this function when run as a script.
-  It is for basic testing.
-  """
+    """
+    The program calls this function when run as a script.
+    It is for basic testing.
+    """
 
-  my_class = C()
-  result = my_class.f()
-  print(f"Is None the result: {result}")
+    name_input = input("Type text file name: ")
+    text_file = TxtFile(name_input)
+
+    try:
+        text_file.text_list()
+        text_file.write_text()
+    except FileNotFoundError:
+        pass
+    else:
+        print('done')
 
 
 if __name__ == "__main__":
-  main()
+    main()
