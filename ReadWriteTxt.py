e4cb5cb89fb9b1b6113969683de1600d9e5aaac7
ischmidls <50844075+ischmidls@users.noreply.github.com>
2021-07-05 18:40:13 -0700

    Create ReadWriteTxt.py

---
+++ ReadWriteTxt.py
@@ -0,0 +1,40 @@
+# Author: Izak Schmidlkofer
+# Date(s): 2021
+# Description: class File represents a file; 
+
+
+
+class File:
+  """
+  Represents a C
+  Attributes: file_name: string
+  Methods: f()
+  """
+  
+  def __init__(self, file_name: string):
+    """Returns an object"""
+
+    
+  
+  def f(self):
+    """
+    Takes 
+    Returns
+    """
+
+    return 
+
+
+def main():
+  """
+  The program calls this function when run as a script.
+  It is for basic testing.
+  """
+
+  my_class = C()
+  result = my_class.f()
+  print(f"Is None the result: {result}")
+
+
+if __name__ == "__main__":
+  main()
